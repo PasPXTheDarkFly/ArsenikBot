ef936a779bdaf5dfb5d6e91e191f4b53d750b722
PasPXTheDarkFly <85247966+PasPXTheDarkFly@users.noreply.github.com>
2022-03-24 21:02:31 +0100

    3 commit

--- index.js
+++ index.js
@@ -1,26 +1,50 @@
-const { Client, MessageEmbed } = require("discord.js");
+const { Client, MessageEmbed, Message } = require("discord.js");
 const PREFIX ="!!";
 const client = new Client({
     intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", ]
 });
+
+
 
 const embedKoth1h = new MessageEmbed()
     .setColor("#f00020")
     .setTitle("Koth dans 1h !⚔️")
     .setDescription("Tous ceux qui participeront au koth, merci de cocher la réaction correspondante")
     .setImage("https://i.ibb.co/R2RTjQX/Logo-Asvadia.png")
-    .setTimestamp()
     .setFooter({text: 'Staff de la arsenik', iconURL:'https://i.ibb.co/qFmrsBC/Arsenik-Bot-PP.jpg'})
+
+const embedKoth5min = new MessageEmbed()
+.setColor("#f00020")
+.setTitle("Koth dans 5min !⚔️")
+.setDescription("Tout ceux qui ont réagie, merci de vous connecter au jeu")
+.setImage("https://i.ibb.co/R2RTjQX/Logo-Asvadia.png")
+.setFooter({text: 'Staff de la arsenik', iconURL:'https://i.ibb.co/qFmrsBC/Arsenik-Bot-PP.jpg'})
+
 
 
 setInterval(function(){
     var date = new Date()
     var heure = date.getHours();
     var minutes = date.getMinutes();
-    console.log("ok")
-    if(heure === 18){
-        if(minutes === 0){
+    console.log("check")
+    if(heure === 17){
+        if(minutes === 30){
             client.channels.cache.get(`956241094620905543`).send({embeds: [embedKoth1h]})
+                .then(m =>{
+                    m.react('✅')
+                    m.react('❌')
+                })
+        }
+    }
+}, 60000)
+
+setInterval(function(){
+    var date2 = new Date()
+    var heure2 = date2.getHours();
+    var minutes2 = date2.getMinutes();
+    if(heure2 === 18){
+        if(minutes2 === 25){
+            client.channels.cache.get(`956241094620905543`).send({embeds: [embedKoth5min]})
         }
     }
 }, 60000)
@@ -30,7 +54,7 @@ setInterval(function(){
 client.on("ready" ,() =>{
     client.user.setPresence({
         activities: [{
-            name: "play.asvadia.eu"
+            name: "asvadia.eu"
         }],
         status: "online"
 
@@ -51,8 +75,12 @@ client.on("messageCreate", async message => {
                     .setColor("#f00020")
                     .setTitle("Voici la liste des commandes")
                     .addField("__help__", "Affiche la liste des commandes")
-                    .addField("__code__", "Permet de partager du code")
                     .addField("__candidature__", "Affiche la structure d'une candidature")
+                    .addField("__ban__", "Permet de bannir un membre du serveur")
+                    .addField("__kick__", "Permet de kick un membre du serveur")
+                    .addField("__mute__", "Permet de rendre muet un membre du serveur")
+                    .addField("__unmute__", "Permet de rendre la parole a un membre muet du serveur")
+                    .addField("__tempmute__", "Permet de mute temporairement un membre du serveur")
                     .setTimestamp()
                     .setFooter({text: 'Staff de la arsenik', iconURL:'https://i.ibb.co/qFmrsBC/Arsenik-Bot-PP.jpg'})                   
 
@@ -64,6 +92,7 @@ client.on("messageCreate", async message => {
                 break;
 
             case "candidature":
+
                 const embedCandid = new MessageEmbed()
                     .setColor("#606060")
                     .setTitle("Voici les informations à faire parvenir dans votre candidature")
@@ -76,11 +105,117 @@ client.on("messageCreate", async message => {
                 message.channel.send({embeds: [embedCandid]})
                 break;
 
-            case "koth1h":
+            case "ban":
+                if(message.member.permissions.has("ADMINISTRATOR")){
+                    if(message.content.startsWith(PREFIX + "ban")){
+                        let mention = message.mentions.members.first();
 
-                message.channel.send({embeds: [embedKoth1h]})
-
+                        if(mention == undefined){
+                            message.reply("Membre inexistant");
+                        }
+                        else {
+                            if(mention.bannable){
+                                mention.ban()
+                                message.channel.send(mention.displayName + " à été bannie avec succès")
+                            }
+                            else {
+                                message.reply("Impossible de bannir ce membre")
+                            }
+                        }
+                    }
+                }
+                else{
+                    message.reply("Tu n'as pas la permission d'éxecuter cette commmande")
+                }
                 break;
+
+            case "kick":
+                if(message.member.permissions.has("ADMINISTRATOR")){
+                    if(message.content.startsWith(PREFIX + "kick")){
+                        let mention = message.mentions.members.first();
+
+                        if(mention == undefined){
+                            message.reply("Membre inexistant")
+                        }
+                        else {
+                            if(mention.kickable){
+                                mention.kick();
+                                message.channel.send(mention.displayName + " à été kick avec succès")
+                            }
+                            else {
+                                message.reply("Immposible de kick ce membre")
+                            }
+                        }
+                    }
+                }
+                else{
+                    message.reply("Tu n'as pas la permission d'éxecuter cette commande")
+                }
+            break;
+
+            case "mute":
+                if(message.member.permissions.has("ADMINISTRATOR")){
+                    if(message.content.startsWith(PREFIX + "mute")){
+                        let mention = message.mentions.members.first();
+
+                        if(mention == undefined){
+                            message.reply("Membre inexistant")
+                        }
+                        else {
+                            mention.roles.add("956631211936186398")
+                            message.channel.send(mention.displayName + " à été mute avec succès")
+                        }
+                    }
+                }
+                else{
+                    message.reply("Tu n'as pas la permission d'éxecuter cette commande")
+                }
+            break;
+
+            case "unmute":
+                if(message.member.permissions.has("ADMINISTRATOR")){
+                    if(message.content.startsWith(PREFIX + "unmute")){
+                        let mention = message.mentions.members.first();
+
+                        if(mention == undefined){
+                            message.reply("Membre inexistant")
+                        }
+                        else{
+                            mention.roles.remove("956631211936186398")
+                            message.channel.send(mention.displayName + " à été unmute avec succès")
+                        }
+                    }
+                }
+                else {
+                    message.reply("Tu n'as pas la permission d'éxecuter cette commande")
+                }
+            break;
+
+            case "tempmute":
+                if(message.member.permissions.has("ADMINISTRATOR")){
+                    if(message.content.startsWith(PREFIX + "tempmute")){
+                        let mention = message.mentions.members.first();
+
+                        if(mention == undefined){
+                            message.reply("Membre inexistant")
+                        }
+                        else{
+                            let args = message.content.split(" ");
+                            if (args[2] === ""){
+                                message.reply("Il ne faut pas mettre d'espace entre le temps et la mention")
+                            }
+                            else{
+                                mention.roles.add("956631211936186398");
+                                setTimeout(function() {
+                                    mention.roles.remove("956619643135598622")
+                                    message.channel.send("<@" + mention.id + "> tu peux désormais parler de nouveau")
+                                },args[2] * 1000)
+                            }
+                            
+                        }
+                    }
+                }
+            break;
 
             default:
                 message.reply(`Cette commande n'existe pas, fait "!!help" pour voir la liste des commandes`)
@@ -96,4 +231,4 @@ client.on("messageReactionAdd", async (reaction, user) => {
 
 
 
-client.login(process.env.TOKEN);
+client.login("OTUzNzU4OTE2NzUwMjIxMzUy.YjJPNQ.5XtVhZ0a5iwynC7t6YDn4U8XqFs");
